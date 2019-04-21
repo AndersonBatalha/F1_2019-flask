5340b6fe8980cda7696a446d604f4b52af72137e
AndersonBatalha <andersonpbatalha@gmail.com>
2019-04-21 20:40:14 -0300

    adicionadas rotas para outras views

--- app/views.py
+++ app/views.py
@@ -7,3 +7,27 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/equipes')
+def equipes():
+    return render_template('equipes.html')
+
+@app.route('/pilotos')
+def pilotos():
+    return render_template('pilotos.html')
+
+@app.route('/calendario')
+def calendario():
+    return render_template('calendario.html')
+
+@app.route('/classificacao')
+def classificacao():
+    return render_template('classificacao.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')

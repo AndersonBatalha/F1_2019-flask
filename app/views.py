9b5e02227def2ed3bb51bfec8fa1c4509f4604ff
AndersonBatalha <andersonpbatalha@gmail.com>
2019-04-22 23:16:09 -0300

    Form para página de login

--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template
+# imports
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Email, DataRequired, Length
+
+# config
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+# forms
+
+app.config['SECRET_KEY'] = 'Z}hrdI2xFMN_xc]'
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', [
+        Email(),
+        DataRequired(),
+        Length(min=8, max=40),
+    ])
+    senha = PasswordField('Senha', [
+        DataRequired(),
+        Length(min=5, max=25),
+    ])
+    sessao = BooleanField('Manter conectado')
+    enviar = SubmitField('Login')
+
+# views
 
 @app.route('/')
 def index():
@@ -28,6 +53,10 @@ def classificacao():
 def cadastro():
     return render_template('cadastro.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm(request.form)
+    if request.method == "POST" and  form.validate_on_submit():
+        print('submit')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)

29ea5919bd445baac090dce45514e2e7bf27de82
AndersonBatalha <andersonpbatalha@gmail.com>
2019-04-23 23:12:37 -0300

    formulário de cadastro

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Email, DataRequired, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, IntegerField
+from wtforms.validators import Email, DataRequired, Length, EqualTo
 
 # config
 app = Flask(__name__)
@@ -13,6 +13,35 @@ bootstrap = Bootstrap(app)
 # forms
 
 app.config['SECRET_KEY'] = 'Z}hrdI2xFMN_xc]'
+
+class RegisterForm(FlaskForm):
+    nome = StringField('Nome completo', validators=[
+        DataRequired(),
+        Length(min=5, max=40)
+    ])
+    email = StringField('E-mail', validators=[
+        DataRequired(),
+        Length(min=5, max=30)
+    ])
+    data_nasc = DateField('Data de nascimento')
+    endereco = StringField('Endereço', validators=[ DataRequired() ])
+    numero = IntegerField('Número', default=0)
+    complemento = StringField("Complemento")
+    bairro =  StringField("Bairro", validators=[ DataRequired() ])
+    cidade = StringField("Cidade", validators=[ DataRequired() ])
+    estado = StringField("Estado", validators=[ DataRequired() ])
+    pais = StringField("País", validators=[ DataRequired() ])
+    senha = PasswordField("Senha", validators=[
+        DataRequired(),
+        Length(min=5, max=25)
+    ])
+    confirmacao_senha = PasswordField("Confirme a senha", validators=[
+        DataRequired(),
+        Length(min=5, max=25),
+        EqualTo('senha'),
+    ])
+    cadastrar = SubmitField('Inscreva-se')
+
 
 class LoginForm(FlaskForm):
     email = StringField('E-mail', [
@@ -49,14 +78,16 @@ def calendario():
 def classificacao():
     return render_template('classificacao.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-    return render_template('cadastro.html')
+    form = RegisterForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('cadastro.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == "POST" and  form.validate_on_submit():
-        print('submit')
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
